a2df9acbf7996906334dfbf7d374ddd18015d44e
Damian Mostert <damianmostert86@gmail.com>
2024-03-14 13:54:14 +0200

    Update page.js

--- src/app/[url]/[url2]/page.js
+++ src/app/[url]/[url2]/page.js
@@ -5,6 +5,5 @@ import { Page } from "@components";
 import * as path from "path";
 
 export default function View({ params }) {
-  return <Page url={path.join("/", params.url1, params.url2)} />;
+  return <Page url={path.join("/", params.url, params.url2)} />;
 }
-Ï
