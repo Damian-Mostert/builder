decd5a3ca3c54cc3719eaf37797c6559d75bc44f
Damian Mostert <damianmostert86@gmail.com>
2024-03-14 16:23:50 +0200

    Update buildbody.js

--- src/lib/modules/buildbody.js
+++ src/lib/modules/buildbody.js
@@ -1,31 +1,13 @@
 import Components from "@components";
 
 export function BuildBody({ template, links, mediaLinks, functions }) {
-    return Array.isArray(template) ? (
-        <>
-            {template.map((item, index) => (
-                <BuildBody
-                    functions={functions}
-                    key={index}
-                    links={links}
-                    mediaLinks={mediaLinks}
-                    template={item}
-                    {...item}
-                />
-            ))}
+    if (Array.isArray(template))
+        return <>
+            {template.map((item, index) => <BuildBody functions={functions} key={index} links={links} mediaLinks={mediaLinks} template={item} {...item} />)}
         </>
-    ) : template && (function(Component){
-                <Component
-            {...template.__props}
-            functions={functions}
-            links={links}
-            mediaLinks={mediaLinks}
-            children={BuildBody({
-                template: template.children,
-                links,
-                mediaLinks,
-                functions,
-            }(Components[template.__component]))}
-        />
-    }());
+
+    if (template) {
+        const C = Components[template.__component];
+        return <C {...template.__props} functions={functions} links={links} mediaLinks={mediaLinks} children={BuildBody({ template: template.children, links, mediaLinks, functions })} />
+    }
 }
